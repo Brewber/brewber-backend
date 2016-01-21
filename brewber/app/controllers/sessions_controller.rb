1dcf78e43d17c46cefcde474e4ebb27ecb2881c8
Connor Hailey <connorhailey@gmail.com>
2016-01-21 13:01:13 -0500

    changes to user_params

--- brewber/app/controllers/sessions_controller.rb
+++ brewber/app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    user = User.find_by(email: params[:session][:email].downcase)
+    user = User.find_by(phone_number: params[:session][:phone_number].downcase)
     if user && user.authenticate(params[:session][:password])
     	log_in user
     	return render status: 200, json: { user_info: user.to_json}
