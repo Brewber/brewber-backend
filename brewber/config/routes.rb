e135659ae1aaa1c1658f0834994e7e4f69b4f63a
sysod <drshrey@sysods-Air.home>
2016-02-07 01:37:46 -0500

    implementing credentials abstraction

--- brewber/config/routes.rb
+++ brewber/config/routes.rb
@@ -8,10 +8,6 @@ Rails.application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-  post    'users'  => 'users#create'
-  post   'login'   => 'sessions#create'
-  delete 'logout'  => 'sessions#destroy'
-
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
